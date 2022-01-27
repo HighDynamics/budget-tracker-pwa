a1b12d36940d4c11cf03c070a9cfd2d3ea56b66b
Daniel Denton <danielryandenton@gmail.com>
2022-01-27 13:43:16 -0600

    indexedDB invoked

---
+++ public/js/idb.js
@@ -0,0 +1,68 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('new_payment', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadPayment();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_payment'], 'readwrite');
+
+  const paymentObjectStore = transaction.objectStore('new_payment');
+
+  paymentObjectStore.add(record);
+}
+
+function uploadPayment() {
+  const transaction = db.transaction(['new_payment'], 'readwrite');
+
+  const paymentObjectStore = transaction.objectStore('new_payment');
+
+  const getAll = paymentObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_payment'], 'readwrite');
+
+          const paymentObjectStore = transaction.objectStore('new_payment');
+
+          paymentObjectStore.clear();
+
+          alert('All saved transactions have been submitted');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadPayment);
